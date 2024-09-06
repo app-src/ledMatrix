f97164b5710d4fd8a23b3235aaf10a8cdd8faf2e
Macle57 <adityagoel2462@gmail.com>
2024-09-06 14:54:28 +0530

    arragned code into functions

--- ledmatrixPrinter.py
+++ ledmatrixPrinter.py
@@ -20,42 +20,61 @@ esp32_ip = "172.5.0.106"
 img = Image.open('star.jpg') #use any jpg that is of 35 x 30 pixels (width x height)
 data= list(img.getdata())
 
-data2d = []
 
 # set pixel data to 0 if total rgb luminance is too bright 
-for index, pixel in enumerate(data):
-    print(pixel)
-    if mean(pixel) > 250:
-        data[index] = (0,0,0)
+def convertWhiteToBlack(data, threshold):
+    for index, pixel in enumerate(data):
+        if mean(pixel) > threshold:
+            data[index] = (0,0,0)
 
 #convert data to 2d array
-for n in range(0, (len(data)), ORIGWIDTH):
-    data2d.append(list(data[n: n+ORIGWIDTH]))
-# print("Height??: " ,len(data2d))
-# print("Width??: ", len(data2d[0]))
+def return2dArray(data, rowSize=ORIGWIDTH):
 
-for i in range(1, len(data2d), 2):
-    #reverse the array
-    data2d[i]=data2d[i][::-1]
+    data2d = []
+    for n in range(0, (len(data)), rowSize):
+        data2d.append(list(data[n: n+rowSize]))
+    return data2d
+    # print("Height??: " ,len(data2d))
+    # print("Width??: ", len(data2d[0]))
+
+def return1dArray(data2d):
+    data1d = []
+    for row in data2d:
+        data1d.extend(row)
+    return data1d
+
+def reverseEveryOtherRow(data2d):
+    for i in range(1, len(data2d), 2):
+        #reverse the array
+        data2d[i]=data2d[i][::-1]
  
-print(data2d)
-
-#sends continious stream to simulate upwards scroll on the led matrix of the entire picture    
-# for n in range(len(data2d)-SKIPINTERVAL):
-#     print("sneding")
-#     screenInstance = []
-#     for row in data2d[n:n+SKIPINTERVAL]:
-#         for rgb in row:
-#             screenInstance.append(rgb)
-#     print(screenInstance, end='\n\n')
-#     # temp = Image.new('RGB', (ORIGWIDTH, SKIPINTERVAL))
-#     # temp.putdata(screenInstance)
-#     # temp.save(f'cropped{n}.jpg')
-#     print("\n\n\n\n")
-#     requests.put(url="https://allprojects68-default-rtdb.asia-southeast1.firebasedatabase.app/vaiLED/img.json", json= screenInstance)
-#     time.sleep(UPDATEDELAY)
+ 
+def writeToJson(data1d, filename: str):
+    with open(filename, 'w') as outfile:
+        json.dump(data1d, outfile)
+        print("JSON file created")
     
-print(len(data))
+# print(data2d)
+
+# sends continious stream to simulate upwards scroll on the led matrix of the entire picture    
+def simulate_scroll(data2d, SKIPINTERVAL=SKIPINTERVAL, UPDATEDELAY=UPDATEDELAY):
+    for n in range(len(data2d)-SKIPINTERVAL):
+        print("sneding")
+        screenInstance = []
+        for row in data2d[n:n+SKIPINTERVAL]:
+            for rgb in row:
+                screenInstance.append(rgb)
+        print(screenInstance, end='\n\n')
+        # temp = Image.new('RGB', (ORIGWIDTH, SKIPINTERVAL))
+        # temp.putdata(screenInstance)
+        # temp.save(f'cropped{n}.jpg')
+        print("\n\n\n\n")
+        requests.put(url="https://allprojects68-default-rtdb.asia-southeast1.firebasedatabase.app/vaiLED/img.json", json= screenInstance)
+        time.sleep(UPDATEDELAY)
+        
+        
+
+
 #dump data into a json
 
 def send_data(data, esp32_ip):
@@ -67,11 +86,7 @@ def send_data(data, esp32_ip):
         print("Response:", response.text)
     except requests.exceptions.RequestException as e:
         print("Error sending data:", e)
-
-
-with open('output2.json', 'w') as outfile:
-    json.dump(data2d, outfile)
-    print("JSON file created")
-
-
-send_data(data_to_send, esp32_ip)    
+        
+data2d = return2dArray(data)
+reverseEveryOtherRow(data2d)
+send_data(return1dArray(data2d), esp32_ip)
